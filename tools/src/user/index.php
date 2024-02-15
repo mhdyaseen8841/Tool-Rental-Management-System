fc02176481b5d1839a7e51d74fafc308b8b16995
mhdyaseen8841 <riyaskh123@gmail.com>
2024-02-16 00:04:42 +0530

    whats app page update

--- tools/src/user/index.php
+++ tools/src/user/index.php
@@ -92,20 +92,95 @@ if (isset($_REQUEST["cId"])) {
         <div class="container-fluid">
             <div class="container-fluid">
                 <!-- Title -->
-                <div class=" d-flex justify-content-between align-items-center py-3">
-                    <div>
-                        <h2 class="h2 mb-0 text-primary"> Aone Rental</h2>
-                        <h5 class="h5 mb-0"></a>Pukkattupady</h5>
+                <div class="row justify-content-between pt-2 pb-1">
+                    <div class="col-xs-2 col-sm-4">
+                        <div class="row justify-content-between">
+                            <div class="col-6 col-sm-12">
+                                <div class="h3 mb-0 text-primary text-start"> Aone Rental</div>
+                            </div>
+                            <div class="col-6 col-sm-12">
+                                <div class="h3 mb-0 text-end"></a>Pukkattupady</div>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <h2 class="h4 mb-0 text-end"><?php echo $rs[0]["cName"]; ?></h2>
-                        <div class="text-end"><span class="badge bg-danger rounded-pill ">PENDING AMOUNT</span> <b class="text-danger ms-1 h4"><?php echo "₹" . abs($itemAmount - $paid)  ?></b></div>
-                        <div class="text-end"><span class="badge bg-primary rounded-pill ">DAILY AMOUNT</span> <b class="text-primary ms-1 h4"><?php echo "₹" . $result2 ?></b></div>
+                    <div class="col-xs-6 col-sm-4 col-lg-3 mt-2 mt-md-0 px-2">
+                        <h2 class="h4 mb-0 text-secondary text-center text-md-end"><?php echo $rs[0]["cName"]; ?></h2>
+                        <div class="row">
+                            <div class="col-xs-8 col-sm-6">
+                                <div class="vstack text-end">
+                                    <span class="badge bg-danger rounded-pill Display 6">PENDING AMOUNT</span>
+                                    <b class="text-danger text-center h3"><?php echo "₹" . number_format((float)abs($itemAmount - $paid), 2, '.', '');  ?></b>
+                                </div>
+                            </div>
+                            <div class="col-xs-8 col-sm-6">
+                                <div class="vstack text-end">
+                                    <span class="badge bg-primary rounded-pill Display 6">DAILY AMOUNT</span>
+                                    <b class="text-primary text-center h3"><?php echo "₹" . number_format((float)$result2, 2, '.', ''); ?></b>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
                 <!-- Main content -->
                 <div class="row mt-4">
+                    <div class="col-12">
+                        <!-- Customer Notes -->
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                <div class="row justify-content-between">
+
+                                    <div class="col-6">
+                                        <h3>Items</h3>
+                                    </div>
+                                    <div class="table-responsive" style="height: 370px;">
+                                        <table class="table w-100">
+                                            <h4 id="itemTableFoot"></h4>
+                                            <thead>
+                                                <tr style="background : white; position: sticky; top: 0;z-index:99;">
+                                                    <th scope="col"> </th>
+                                                    <?php
+                                                    foreach ($it as $key => $value) { ?>
+                                                        <th style="text-align: center" scope="col"><?php echo  $value->name; ?></th>
+                                                    <?php
+                                                    }
+                                                    ?>
+                                                </tr>
+                                            </thead>
+                                            <tbody id="itemTable">
+                                                <?php
+                                                foreach ($itData as $key => $value) {
+                                                    echo "<tr>";
+                                                    foreach ($value as $key1 => $value1) {
+                                                        if ($key1 == 0) {
+                                                            echo "<td style='background : white; left : 0; position : sticky; z-index:98;' align ='left' nowrap='nowrap'>" . $value1 . "</td>";
+                                                        } else {
+                                                            $income = $value1->incoming;
+                                                            $outgo = $value1->outgoing;
+                                                            echo "<td align='center'>";
+                                                            if ($income->qty != 0) {
+                                                                echo "<div style='background:green;width:50px;color:white;font-weight:bold;'>" . $income->qty . "</div>";
+                                                            }
+                                                            if ($outgo->qty != 0) {
+                                                                echo "<div style='background:red;width:50px;color:white;font-weight:bold;'>" . $outgo->qty . "</div>";
+                                                            }
+                                                            echo "</td>";
+                                                        }
+                                                    }
+                                                    echo "</tr>";
+                                                } ?>
+                                                <tr>
+                                                    <td></td>
+                                                    <td></td>
+                                                </tr>
+                                            </tbody>
+
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div class="col-lg-4">
                         <!-- Details -->
 
@@ -136,144 +211,6 @@ if (isset($_REQUEST["cId"])) {
                                                     </tr>
                                                 <?php } ?>
                                             </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <!-- Customer Notes -->
-                        <div class="card mb-4">
-                            <div class="card-body">
-                                <h3>Items Amount</h3>
-                                <div class="table-responsive">
-                                    <table class="table">
-                                        <thead>
-                                            <tr style="background : white; position: sticky; top: 0;">
-                                                <th scope="col">#</th>
-                                                <th scope="col">Item</th>
-                                                <th scope="col">Amount</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php
-                                            $i = 0;
-                                            foreach ($items as $value) {
-                                                $i++;
-                                            ?>
-                                                <tr>
-                                                    <th scope="row"><?php echo $i; ?></th>
-                                                    <td><?php echo $value->itemName; ?></td>
-                                                    <td><?php echo $value->amount; ?></td>
-                                                </tr>
-                                            <?php } ?>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="card mb-4">
-                            <div class="card-body">
-                                <h3>Extra Amount</h3>
-                                <div class="table-responsive">
-                                    <table class="table">
-                                        <thead>
-                                            <tr style="background : white; position: sticky; top: 0;">
-                                                <th scope="col">#</th>
-                                                <th scope="col">Amount</th>
-                                                <th scope="col">Date</th>
-                                                <th scope="col">Note</th>
-                                                <th scope="col">status</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php
-                                            $i = 0;
-                                            foreach ($extraPayment as $value) {
-                                                $i++;
-                                            ?>
-                                                <tr>
-                                                    <th scope="row"><?php echo $i; ?></th>
-                                                    <td><?php echo $value->amount; ?></td>
-                                                    <td nowrap='nowrap'><?php echo $value->date; ?></td>
-                                                    <td><?php echo $value->note; ?></td>
-                                                    <td><?php if ($value->status == 0) { ?>
-                                                            <span class="badge bg-success  rounded-pill">Discount</span>
-                                                        <?php } else { ?>
-                                                            <span class="badge bg-danger  rounded-pill">Add on</span>
-                                                        <?php } ?>
-                                                    </td>
-                                                </tr>
-                                            <?php } ?>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-12">
-                        <!-- Customer Notes -->
-                        <div class="card mb-4">
-                            <div class="card-body">
-                                <div class="row justify-content-between">
-
-                                    <div class="col-6">
-                                        <h3>Items</h3>
-                                    </div>
-
-
-                                    <div class="table-responsive" style="max-height:500px;height: 300px;">
-                                        <table class="table w-100">
-                                            <h4 id="itemTableFoot"></h4>
-                                            <thead>
-                                                <tr style="background : white; position: sticky; top: 0;z-index:99;">
-                                                    <th scope="col"> </th>
-                                                    <?php
-                                                    foreach ($it as $key => $value) { ?>
-                                                        <th style="text-align: center" scope="col"><?php echo  $value->name; ?></th>
-                                                    <?php
-                                                    }
-                                                    ?>
-                                                </tr>
-                                            </thead>
-                                            <tbody id="itemTable">
-                                                <?php
-                                                $pendings = array(); 
-                                                foreach ($itData as $key => $value) {
-                                                    echo "<tr>";
-                                                    foreach ($value as $key1 => $value1) {
-                                                        if ($key1 == 0) {
-                                                            echo "<td style='background : white; left : 0; position : sticky; z-index:98;' align ='left' nowrap='nowrap'>" . $value1 . "</td>";
-                                                        } else {
-                                                            $income = $value1->incoming;
-                                                            $outgo = $value1->outgoing;
-                                                            $pendings[$key1] = $pendings[$key1] + ($outgo->qty - $income->qty);
-                                                            echo "<td align='center'>";
-                                                            if ($income->qty != 0) {
-                                                                echo "<div style='background:green;width:50px;color:white;font-weight:bold;'>" . $income->qty . "</div>";
-                                                            }
-                                                            if ($outgo->qty != 0) {
-                                                                echo "<div style='background:red;width:50px;color:white;font-weight:bold;'>" . $outgo->qty . "</div>";
-                                                            }
-                                                            echo "</td>";
-                                                        }
-                                                    }
-                                                    echo "</tr>";
-                                                } ?>
-                                                <?php
-                                                    echo "<tr style='background : white; position: sticky; bottom: 0;z-index:99;'>";
-                                                    echo "<th>Pending</th>";
-                                                    foreach ($pendings as $key => $value) {
-                                                        echo "<th style='font-weight:bold;text-align:center; font-size:16px'>".$value."</th>";
-                                                    }
-                                                ?>
-                                                </tr>
-                                            </tbody>
-
                                         </table>
                                     </div>
                                 </div>
